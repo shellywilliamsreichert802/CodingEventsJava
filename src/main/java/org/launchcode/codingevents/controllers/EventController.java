324185418752dae39d2af11e264af6df7e6a0f43
shellywilliamsreichert802 <shellyreichert802@gmail.com>
2024-05-12 21:37:42 -0500

    CH 14 CodingEvents complete with Models Binding

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,11 +3,8 @@ package org.launchcode.codingevents.controllers;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,10 +48,10 @@ public class EventController {
 
     //lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName,
-                                         @RequestParam String eventDescription) {
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
 //        EventData.add(new Event(eventName, eventDescription));
-        EventData.add(new Event(eventName, eventDescription));
+//        EventData.add(new Event(eventName, eventDescription));
+        EventData.add(newEvent);
         return "redirect:/events";
     }
 
